f78100610e331e6fa6632e0d655913e8817efed8
Szendi <szendiattila@gmail.com>
2025-02-21 16:15:29 +0100

    seed db if empty on every request.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,29 @@
 
 namespace App\Providers;
 
+use App\Models\Client;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        $this->seedDatabaseWhenEmpty();
+    }
+
+    public function seedDatabaseWhenEmpty(): void
+    {
+        if (app()->environment('local', 'development') && Client::query()->count() === 0) {
+            Artisan::call('db:seed');
+        }
     }
 }
